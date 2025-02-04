1bbd2869515ac042e6e4b517af5f8218026db8c9
Izaskun Mallona <izaskun.mallona@gmail.com>
2025-02-04 11:50:51 +0100

    3-way-diffing

--- 01_hyperfine_and_diff_backends.sh
+++ 01_hyperfine_and_diff_backends.sh
@@ -3,7 +3,7 @@
 
 ## with singularity
 
-hyperfine 'ob run benchmark -b Clustering_singularity.yml --threads 5 --local' --runs 1 --export-csv singularity_1_hyperfine.txt
+hyperfine 'ob run benchmark -b Clustering_singularity.yml --threads 15 --local' --runs 1 --export-csv singularity_1_hyperfine.txt
 
 mv out out_singularity
 
@@ -14,7 +14,7 @@ Rscript -e "rmarkdown::render('00_clustering_metrics_overview.Rmd', \
 
 ## with envmodules
 
-hyperfine 'ob run benchmark -b Clustering_envmodules.yml --threads 5 --local' --runs 1 --export-csv envmodules_1_hyperfine.txt
+hyperfine 'ob run benchmark -b Clustering_envmodules.yml --threads 15 --local' --runs 1 --export-csv envmodules_1_hyperfine.txt
 
 mv out out_envmodules
 
@@ -25,7 +25,7 @@ Rscript -e "rmarkdown::render('00_clustering_metrics_overview.Rmd', \
 
 ## with conda
 
-hyperfine 'ob run benchmark -b Clustering_conda.yml --threads 5 --local' --runs 1 --export-csv conda_1_hyperfine.txt
+hyperfine 'ob run benchmark -b Clustering_conda.yml --threads 15 --local' --runs 1 --export-csv conda_1_hyperfine.txt
 
 mv out out_conda
 
@@ -34,5 +34,24 @@ Rscript -e "rmarkdown::render('00_clustering_metrics_overview.Rmd', \
                   out_csv = 'out_conda_metrics.csv'))"
 
 
-diff out_singularity_metrics.csv out_envmodules_metrics.csv
-diff out_conda_metrics.csv out_envmodules_metrics.csv
+diff --color out_singularity_metrics.csv out_envmodules_metrics.csv
+diff --color out_conda_metrics.csv out_envmodules_metrics.csv
+diff --color out_conda_metrics.csv out_singularity_metrics.csv
+
+
+for fn in $( find out_conda -name "clustbench.scores.gz")
+do
+    # echo $fn
+    agnostic=$(echo $fn | sed 's/out_conda//g')
+    echo $agnostic
+    echo 'conda'
+    zcat out_conda/$agnostic
+    echo 'singularity'
+    zcat out_singularity/$agnostic
+    echo 'envmodules'
+    zcat out_envmodules/$agnostic
+    echo 'diff3'
+    diff3 -A  <(zcat out_conda/$agnostic) <(zcat out_singularity/$agnostic) <(zcat out_envmodules/$agnostic)
+    echo
+    echo
+done
