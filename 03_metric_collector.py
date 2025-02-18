bd0415166db881ce355749713c0456502452f7a0
Izaskun Mallona <izaskun.mallona@gmail.com>
2025-02-18 10:38:51 +0100

    Add config

--- 03_metric_collector.py
+++ 03_metric_collector.py
@@ -13,11 +13,11 @@ import argparse
 import os.path as op
 import subprocess
 
-def process(output_dir, files, report_basename):
+def process(output_dir, metrics_metafile, report_basename):
     subprocess.run(
         ["Rscript", "-e", "rmarkdown::render('04_metric_collector.Rmd', \
                   param=list(input_files='%s', \
-                  out_html = '%s'))" %(files,  op.join(output_dir, report_basename)) ],
+                  out_html = '%s'))" %(metrics_metafile,  op.join(output_dir, report_basename)) ],
         cwd = output_dir,
     )
     
@@ -25,16 +25,18 @@ def main():
     parser = argparse.ArgumentParser(description='Run method on files.')
 
     parser.add_argument('--output_dir', type=str, help='Output directory.')
-    parser.add_argument('--report_basename', type=str, help='HTML report basename.')
     parser.add_argument('--metrics.scores', type=str, nargs='+', help='Metrics files.')
     
     args, _ = parser.parse_known_args()
 
-    files = getattr(args, 'metrics.mapping')
+    files = getattr(args, 'metrics.scores')
+    print(files)
     output_dir = getattr(args, 'output_dir')
-    report_bn = getattr(args, 'report_basename')
+
+    with open('inputs.txt', 'w') as fh:
+        fh.write("\n".join(map(str, files)))
     
-    process(output_dir, files, report_bn)
+    process(output_dir, metrics_metafile = 'inputs.txt', report_basename = 'plotting_report.html')
 
 
 if __name__ == "__main__":
